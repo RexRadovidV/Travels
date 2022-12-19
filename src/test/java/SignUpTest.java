8f9cbd62e75d19854c6736e87563752c436604eb
Wojtek Rachoń <wojtek.rachon@gmail.com>
2022-12-19 09:31:05 +0100

    Set random email

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -25,6 +25,22 @@ public class SignUpTest {
                 .findFirst()
                 .ifPresent(WebElement::click);
 
+        String lastName="Rachoń";
+        int randomNumbers=(int) (Math.random()*1000);
+        String email="tester"+randomNumbers+"@gmail.com";
+
         driver.findElements(By.xpath("//a[text()='  Sign Up']")).get(1).click();
+        driver.findElement(By.name("firstname")).sendKeys("Wojciech");
+        driver.findElement(By.name("lastname")).sendKeys("Rachoń");
+        driver.findElement(By.name("phone")).sendKeys("111111111");
+        driver.findElement(By.name("email")).sendKeys(email);
+        driver.findElement(By.name("password")).sendKeys("Test123");
+        driver.findElement(By.name("confirmpassword")).sendKeys("Test123");
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
+        WebElement heading = driver.findElement(By.xpath("//h3[@class='RTL']"));
+
+        Assert.assertTrue(heading.getText().contains(lastName));
+        Assert.assertEquals(heading.getText(),"Hi, Wojciech Rachoń");
+
     }
 }
